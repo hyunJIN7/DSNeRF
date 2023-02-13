ead68a740df1284436cde5201625141dd6a5f26c
Hyun Jim Kim <35680342+hyunJIN7@users.noreply.github.com>
2023-02-13 09:47:56 +0900

    Update imgs2poses.py

--- imgs2poses.py
+++ imgs2poses.py
@@ -6,13 +6,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--match_type', type=str, 
 					default='exhaustive_matcher', help='type of matcher used.  Valid options: \
 					exhaustive_matcher sequential_matcher.  Other matchers not supported at this time')
-parser.add_argument('scenedir', type=str,
+parser.add_argument('--scenedir', type=str,
                     help='input scene directory')
 args = parser.parse_args()
 
 if args.match_type != 'exhaustive_matcher' and args.match_type != 'sequential_matcher':
 	print('ERROR: matcher type ' + args.match_type + ' is not valid.  Aborting')
 	sys.exit()
+"""
+conda activate DSNeRF-main
+python imgs2poses.py --scenedir=data/yaxis/train
+python imgs2poses.py --scenedir=data/test1
+"""
 
 if __name__=='__main__':
-    gen_poses(args.scenedir, args.match_type)
+	gen_poses(args.scenedir, args.match_type)
